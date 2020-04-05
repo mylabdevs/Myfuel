8b41f92b48782119f17eaac99f70b0e128e625ef
Jairo <jaironsousa@gmail.com>
2020-04-05 10:19:17 -0300

    Criação projeto

---
+++ src/main/java/com/mylabs/myfuel/MyfuelApplication.java
@@ -0,0 +1,13 @@
+package com.mylabs.myfuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfuelApplication.class, args);
+	}
+
+}
